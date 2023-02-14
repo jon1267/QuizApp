d6f5439466c52e4ef585e3b2e98254f754657ee4
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-02-14 12:13:00 +0200

    add json file with data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
+<script setup>
+   import q from "./data/quizes.json"
+   import {ref} from "vue"
+
+   const quizes = ref(q)
+   const search = ref("")
+</script>
+
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" placeholder="Search...">
+      <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card">
-        
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -35,6 +48,36 @@
     background-color: rgba(128,128,128,0.1);
     padding: 10px;
     border-radius: 5px;
-    font-size: 12.5pt;
   }
+
+  .options-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 40px;
+  }
+
+  /* CARD */
+  .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+  }
+
+  .card .card-text {
+    padding: 0 5px;
+  }
+
+  .card .card-text h2 {
+    font-weight: bold;
+  }  
 </style>
